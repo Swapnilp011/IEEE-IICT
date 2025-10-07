b37b18f63af06061697745b141cf3bbdd36e8249
IEEE MGMU <ieeemgmu@gmail.com>
2025-10-07 22:43:46 +0000

    yes

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,10 @@
+
+import AdminSection from '@/components/sections/admin-section';
+
+export default function AdminPage() {
+    return (
+        <div>
+            <AdminSection />
+        </div>
+    )
+}
