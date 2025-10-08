614b722d8d0f9fbaf417e10f15608f4771bdd2f7
Mateen Shadab <itsmateen69@gmail.com>
2025-10-08 09:31:38 +0000

    redesign it like other persons (_for element <TeamMemberCard>_)

--- src/components/sections/team-section.tsx
+++ src/components/sections/team-section.tsx
@@ -98,7 +98,7 @@ export default function TeamSection() {
             <div className="mt-8 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 justify-items-center">
                 {coreCommittee.map((member) => (
                     <div key={member.id} className="h-full">
-                        <TeamMemberCard member={member} />
+                        <TeamMemberCard member={member} size="large" />
                     </div>
                 ))}
             </div>
@@ -110,7 +110,7 @@ export default function TeamSection() {
             <div className="mt-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 justify-items-center">
                 {technicalHeads.map((member) => (
                     <div key={member.id} className="h-full">
-                        <TeamMemberCard member={member} />
+                        <TeamMemberCard member={member} size="large" />
                     </div>
                 ))}
             </div>
